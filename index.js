e82b2b0b7fedfc349fa642ef505c49bb64cd8eea
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-08-08 02:27:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const seekBar = document.getElementById("seek-bar");
+  const startTimeDisplay = document.getElementById("start-time");
+  const endTimeDisplay = document.getElementById("end-time");
+  const playButton = document.getElementById("play-button");
+  let animationInterval;
+  let currentTime = 0;
+  const duration = 242;
+  let isPlaying = false;
+
+  function updateTimeDisplay() {
+    const currentTimeFormatted = formatTime(currentTime);
+    startTimeDisplay.textContent = currentTimeFormatted;
+    const remainingTimeFormatted = formatTime(duration - currentTime);
+    endTimeDisplay.textContent = remainingTimeFormatted;
+  }
+
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    return `${minutes
+      .toString()
+      .padStart(2, "0")}:${secondsLeft.toString().padStart(2, "0")}`;
+  }
+
+  playButton.addEventListener("click", function () {
+    if (isPlaying) {
+      clearInterval(animationInterval);
+      animationInterval = null;
+      playButton.classList.remove("fa-circle-pause");
+      playButton.classList.add("fa-circle-play");
+    } else {
+      let width = (currentTime / duration) * 100;
+      animationInterval = setInterval(() => {
+        width += 0.1;
+        currentTime = Math.floor((width / 100) * duration);
+        if (width <= 100) {
+          seekBar.style.width = width + "%";
+          updateTimeDisplay();
+        } else {
+          clearInterval(animationInterval);
+          animationInterval = null;
+          playButton.classList.remove("fa-circle-play");
+          playButton.classList.add("fa-circle-pause");
+          currentTime = 0;
+          seekBar.style.width = "0%";
+          updateTimeDisplay();
+        }
+      }, (duration * 10) / 100);
+      playButton.classList.remove("fa-circle-play");
+      playButton.classList.add("fa-circle-pause");
+    }
+    isPlaying = !isPlaying;
+  });
+});
