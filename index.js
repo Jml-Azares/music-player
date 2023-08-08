f4addbdbe30e816031a8680d82c531e147c32ffb
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-08-09 02:49:54 +0800

    added song details and lyrics using  API

--- index.js
+++ index.js
@@ -1,64 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const seekBar = document.getElementById("seek-bar");
-  const startTimeDisplay = document.getElementById("start-time");
-  const endTimeDisplay = document.getElementById("end-time");
-  const playButton = document.getElementById("play-button");
-  let animationInterval;
-  let currentTime = 0;
-  const duration = 242;
-  let isPlaying = false;
+const lyrics = document.querySelector(".lyrics-container > p");
+const songThumbnail = document.querySelector(".player-container > img");
+const songTitle = document.querySelector(".player-container > h2");
+const songArtist = document.querySelector(".player-container > p");
 
-  function updateTimeDisplay() {
-    const currentTimeFormatted = formatTime(currentTime);
-    startTimeDisplay.textContent = currentTimeFormatted;
-    const remainingTimeFormatted = formatTime(duration - currentTime);
-    endTimeDisplay.textContent = remainingTimeFormatted;
+const getAPI = async () => {
+  try {
+    const songDetails = {
+      method: "GET",
+      url: "https://genius-song-lyrics1.p.rapidapi.com/song/details/",
+      params: { id: "1393981" },
+      headers: {
+        "X-RapidAPI-Key": "6a16507df5msh23d164fb0009391p1dce14jsn96546fdbc5a8",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+      },
+    };
+
+    const lyricsApi = {
+      method: "GET",
+      url: "https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/",
+      params: { id: "1393981" },
+      headers: {
+        "X-RapidAPI-Key": "6a16507df5msh23d164fb0009391p1dce14jsn96546fdbc5a8",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+      },
+    };
+
+    const [songData, lyricsData] = await Promise.all([
+      axios(songDetails),
+      axios(lyricsApi),
+    ]);
+
+    const thumbnailUrl = songData.data.song.header_image_thumbnail_url;
+    const songTitle = songData.data.song.title;
+    const artistName = songData.data.song.artist_names;
+    const songLyrics = lyricsData.data.lyrics.lyrics.body.html;
+
+    songThumbnail.src = thumbnailUrl;
+    songTitle.textContent = songTitle;
+    songArtist.textContent = artistName;
+    lyrics.innerHTML = songLyrics;
+  } catch (err) {
+    console.error(err);
   }
+};
 
-  function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const secondsLeft = seconds % 60;
-    return `${minutes
-      .toString()
-      .padStart(2, "0")}:${secondsLeft.toString().padStart(2, "0")}`;
-  }
-
-  function togglePlayButton() {
-    if (isPlaying) {
-      playButton.classList.remove("fa-circle-play");
-      playButton.classList.add("fa-circle-pause");
-    } else {
-      playButton.classList.remove("fa-circle-pause");
-      playButton.classList.add("fa-circle-play");
-    }
-  }
-
-  playButton.addEventListener("click", function () {
-    if (isPlaying) {
-      clearInterval(animationInterval);
-      animationInterval = null;
-      isPlaying = false;
-      togglePlayButton();
-    } else {
-      let width = (currentTime / duration) * 100;
-      animationInterval = setInterval(() => {
-        width += 0.1;
-        currentTime = Math.floor((width / 100) * duration);
-        if (width <= 100) {
-          seekBar.style.width = width + "%";
-          updateTimeDisplay();
-        } else {
-          clearInterval(animationInterval);
-          animationInterval = null;
-          currentTime = 0;
-          seekBar.style.width = "0%";
-          updateTimeDisplay();
-          isPlaying = false;
-          togglePlayButton();
-        }
-      }, (duration * 10) / 100);
-      isPlaying = true;
-      togglePlayButton();
-    }
-  });
-});
+getAPI();
